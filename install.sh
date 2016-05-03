3d746ae09eae4cf432a22409877c2d3a50158342
Daniel Costa <maz_dcosta@uolinc.com>
2016-05-03 14:15:56 -0300

    Updating config files

--- install.sh
+++ install.sh
@@ -3,47 +3,41 @@
 DOTFILES_PATH=$(pwd)
 
 function _link_git(){
-    echo "Configurando o git..."
-
-    if [ ! -f ${HOME}/.gitconfig ]; then
-        ln -s ${DOTFILES_PATH}/.gitconfig ${HOME}/.gitconfig
-    fi
-    if [ ! -f ${HOME}/.gitignore ]; then
-        ln -s ${DOTFILES_PATH}/.gitignore ${HOME}/.gitignore
-    fi
-    git config --global include.path ${CONFIG_PATH}/.gitconfig
+  echo "Configurando o git..."
+  git config --global include.path ${DOTFILES_PATH}/.gitconfig
+  git config --global core.excludesfile ${DOTFILES_PATH}/.gitignore
 }
 
 function _get_name(){
-    echo "Digite o nome para o author dos commits:"
-    read name
+  echo "Digite o nome do author dos commits:"
+  read name
 
-    if [ ! -z "$name" ]; then
-       git config --global user.name "${name}"
-    else
-        _get_name
-    fi
+  if [ ! -z "$name" ]; then
+    git config --global user.name "${name}"
+  else
+    _get_name
+  fi
 }
 
 function _get_email(){
-    echo "Digite o e-mail para o author dos commits:"
-    read email
+  echo "Digite o e-mail do author dos commits:"
+  read email
 
-    if [ ! -z "$email" ]; then
-       git config --global user.email "${email}"
-    else
-        _get_email
-    fi
+  if [ ! -z "$email" ]; then
+    git config --global user.email "${email}"
+  else
+    _get_email
+  fi
 }
 
 function _install_myvimrc(){
-    cd ~
-    git clone https://github.com/arthurprogramming/myvimconfig.git
-    ln -s myvimconfig/.vimrc .vimrc
-    ln -s myvimconfig/.vim .vim
-    cd myvimconfig
-    git submodule init
-    git submodule update
+  cd ~
+  git clone https://github.com/arthurprogramming/myvimconfig.git
+  ln -s myvimconfig/.vimrc .vimrc
+  ln -s myvimconfig/.vim .vim
+  cd myvimconfig
+  git submodule init
+  git submodule update
 }
 
 _link_git
