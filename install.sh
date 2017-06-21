a50fcd465c2c6c85fc2a2cfc4a3bb14bdf974b8b
Daniel Costa <daniel.costa@sumup.com>
2017-06-21 16:44:20 -0300

    Including new functions to configure apps

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 DOTFILES_PATH=$(pwd)
 
 function _link_git(){
-  echo "Configurando o git..."
+  echo "Configuring Git..."
   git config --global include.path ${DOTFILES_PATH}/.gitconfig
   git config --global core.excludesfile ${DOTFILES_PATH}/.gitignore
 }
 
 function _get_name(){
-  echo "Digite o nome do author dos commits:"
+  echo "Please input global git author's name:"
   read name
 
   if [ ! -z "$name" ]; then
@@ -20,7 +20,7 @@ function _get_name(){
 }
 
 function _get_email(){
-  echo "Digite o e-mail do author dos commits:"
+  echo "Please input global git author's e-mail:"
   read email
 
   if [ ! -z "$email" ]; then
@@ -31,10 +31,28 @@ function _get_email(){
 }
 
 function _install_vimrc(){
-  cp includes/.vimrc ~/.vimrc
+  echo "Configuring Vim"
+  echo "source ~/.dotfiles/includes/.vimrc" >> ~/.vimrc
+}
+
+function _install_tmux_conf(){
+  echo "Configuring Tmux"
+  echo "source ~/.dotfiles/includes/.tmux.conf" >> ~/.tmux.conf
+}
+
+function _install_bashrc(){
+  echo "Configuring Bash"
+  echo "source ~/.dotfiles/includes/.bashrc" >> ~/.bashrc
+}
+
+function _finish(){
+  echo "Finished. Now please restart bash session."
 }
 
 _link_git
 _get_name
 _get_email
 _install_vimrc
+_install_tmux_conf
+_install_bashrc
+_finish
