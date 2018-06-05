de14d142f22336031a2def2d123fcb06dae9112c
Daniel Costa <danielcosta@gmail.com>
2018-06-05 15:03:45 -0300

    fixing tmux bootstrap scripts

--- tmux-verify-os.sh
+++ tmux-verify-os.sh
@@ -1,6 +1,6 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     tmux source-file /usr/local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf
-elseif [[ "$OSTYPE" == "linux-gnu" ]]; then
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     tmux source-file /usr/share/powerline/bindings/tmux/powerline.conf
 fi
 
